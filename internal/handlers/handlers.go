0676168a7b63da0b3f3791fdbc70d945c2c5d957
Trevor Sawler <trevor.sawler@gmail.com>
2021-01-29 10:45:17 -0400

    Creating form elements and handling websocket responses/messages

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -28,11 +28,23 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type WebSocketConnection struct {
+	*websocket.Conn
+}
+
 // WsJsonResponse defines the response sent back from websocket
 type WsJsonResponse struct {
 	Action      string `json:"action"`
 	Message     string `json:"message"`
 	MessageType string `json:"message_type"`
+}
+
+// WsPayload defines the websocket request from the client
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
 }
 
 // WsEndpoint upgrades connection to websocket
